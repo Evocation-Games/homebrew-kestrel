2240a667c0da5d4b820f0ce714a00d745fef6718
Tom Hancocks <tomhancocks@gmail.com>
2023-10-18 15:26:04 +0100

    First test formula

---
+++ Formula/kdl.rb
@@ -0,0 +1,15 @@
+class kdl < Formula
+    desc "Kestrel Definition Language compiler"
+    homepage "https://github.com/EvocationGames/KestrelEngine.git"
+    url "https://github.com/EvocationGames/KestrelEngine/releases/download/v0.9/KestrelEngine-macOS.zip"
+    head "https://github.com/EvocationGames/KestrelEngine.git"
+    license "MIT"
+
+    def install
+        bin.install "kdl"
+    end
+
+    test do
+        
+    end
+end
---
+++ README.md
@@ -0,0 +1,1 @@
+# Kestrel Homebrew Formulas
