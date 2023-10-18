8538aeed09774854f2b8cd57461d820b664e8376
Tom Hancocks <tomhancocks@gmail.com>
2023-10-18 15:58:50 +0100

    More testing of Homebrew formula

---
+++ Formula/kestrel.rb
@@ -0,0 +1,16 @@
+class Kestrel < Formula
+    desc "Kestrel Test Player"
+    homepage "https://github.com/EvocationGames/KestrelEngine.git"
+    url "https://github.com/EvocationGames/KestrelEngine/releases/download/v0.9/KestrelEngine-macOS.zip"
+    head "https://github.com/EvocationGames/KestrelEngine.git"
+    license "MIT"
+    sha256 "f25b3d8fe735c93979267af029d87957757fbb7263da17d6ddad5a2f91e8732f"
+
+    def install
+        prefix.install "Kestrel.app"
+    end
+
+    test do
+
+    end
+end
