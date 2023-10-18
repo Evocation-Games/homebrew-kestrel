db11d581f6e61a971310715cc423d72894ab2968
Tom Hancocks <tomhancocks@gmail.com>
2023-10-18 20:35:09 +0100

    Update downloads.

--- Formula/kdl.rb
+++ Formula/kdl.rb
@@ -1,10 +1,10 @@
 class Kdl < Formula
     desc "Kestrel Definition Language compiler"
     homepage "https://github.com/EvocationGames/KestrelEngine.git"
-    url "https://github.com/EvocationGames/KestrelEngine/releases/download/v0.9/KestrelEngine-macOS.zip"
+    url "https://github.com/EvocationGames/KestrelEngine/releases/download/v0.9/KDL-macOS.tar.gz"
     head "https://github.com/EvocationGames/KestrelEngine.git"
     license "MIT"
-    sha256 "f25b3d8fe735c93979267af029d87957757fbb7263da17d6ddad5a2f91e8732f"
+    sha256 "fa034824d1e74313bcee157358d713cde54ca2157057c5d9c6e4f95d96732799"
 
     def install
         bin.install "kdl"
--- Formula/kestrel.rb
+++ Formula/kestrel.rb
@@ -1,10 +1,10 @@
 class Kestrel < Formula
     desc "Kestrel Test Player"
     homepage "https://github.com/EvocationGames/KestrelEngine.git"
-    url "https://github.com/EvocationGames/KestrelEngine/releases/download/v0.9/KestrelEngine-macOS.zip"
+    url "https://github.com/EvocationGames/KestrelEngine/releases/download/v0.9/KestrelEngine-macOS.tar.gz"
     head "https://github.com/EvocationGames/KestrelEngine.git"
     license "MIT"
-    sha256 "f25b3d8fe735c93979267af029d87957757fbb7263da17d6ddad5a2f91e8732f"
+    sha256 "0be71aff25451b24e08be51fc7eaf4c7a90b53cde1105aa8188e538347bef1d1"
 
     def install
         libexec.install "Kestrel.app"
