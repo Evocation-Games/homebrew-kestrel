293b785b83d3e1ab2ca0c65738cd66e6be7be406
Tom Hancocks <tomhancocks@gmail.com>
2023-10-20 11:40:23 +0100

    Fix cf-definitions

--- Formula/cf-definitions.rb
+++ Formula/cf-definitions.rb
@@ -5,6 +5,7 @@ class CfDefinitions < Formula
     head "https://github.com/Evocation-Games/CosmicFrontier-Resource-Types.git"
     license "MIT"
     sha256 "98877b19f1ea9cd1054ba9e796861b6927e2d83d3c9912c7ad261a10e3c6b241"
+    version "1"
 
     def install
         prefix.install Dir["CosmicFrontier"] 
